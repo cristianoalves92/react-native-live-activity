9bb86a43c502164d3b84f06059c47e2c1375a3b1
George Jose <george.jose.a@gmail.com>
2024-02-03 14:39:14 -0500

    Make it not crash on iPhone app running on macOS with apple silicon

--- ios/LiveActivity.swift
+++ ios/LiveActivity.swift
@@ -1,65 +1,69 @@
-import Foundation
 import ActivityKit
+import Foundation
 
 @objc(LiveActivity)
 class LiveActivity: NSObject {
+  func isIphone() -> Bool {
+    if #available(iOS 14.0, *) {
+      return !ProcessInfo.processInfo.isiOSAppOnMac
+    } else {
+      return false
+    }
+  }
 
-    @objc(startActivity:withResolver:withRejecter:)
-    func startActivity(data: String, resolve:RCTPromiseResolveBlock, reject:RCTPromiseRejectBlock) -> Void {
-        if #available(iOS 16.1, *) {
-            var activity: Activity<MyActivityAttributes>?
-            let initialContentState = MyActivityAttributes
-                .ContentState(data: data)
-            let activityAttributes = MyActivityAttributes()
-            
-            do {
-                activity = try Activity
-                    .request(attributes: activityAttributes, contentState: initialContentState)
-                
-                resolve("Requested Live Activity \(String(describing: activity?.id)).")
-            } catch (let error) {
-                reject("Error requesting Live Activity \(error.localizedDescription).", "", error)
-            }
-        } else {
-            reject("Not available", "", NSError())
-        }
+  @objc(startActivity:withResolver:withRejecter:)
+  func startActivity(data: String, resolve: RCTPromiseResolveBlock, reject: RCTPromiseRejectBlock) {
+    if #available(iOS 16.1, *), isIphone() {
+      var activity: Activity<MyActivityAttributes>?
+      let initialContentState = MyActivityAttributes
+        .ContentState(data: data)
+      let activityAttributes = MyActivityAttributes()
+
+      do {
+        activity = try Activity
+          .request(attributes: activityAttributes, contentState: initialContentState)
+
+        resolve("Requested Live Activity \(String(describing: activity?.id)).")
+      } catch {
+        reject("Error requesting Live Activity \(error.localizedDescription).", "", error)
+      }
+    } else {
+      resolve(nil)
     }
-    
-    @objc(listAllActivities:withRejecter:)
-    func listAllActivities(resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
-        if #available(iOS 16.1, *) {
-            var activities = Activity<MyActivityAttributes>.activities
-            activities.sort { $0.id > $1.id }
-            
-            return resolve(activities.map{["id": $0.id, "data": $0.contentState.data ]})
-        } else {
-            reject("Not available", "", NSError())
-        }
+  }
+
+  @objc(listAllActivities:withRejecter:)
+  func listAllActivities(resolve: RCTPromiseResolveBlock, reject _: RCTPromiseRejectBlock) {
+    if #available(iOS 16.1, *), isIphone() {
+      var activities = Activity<MyActivityAttributes>.activities
+      activities.sort { $0.id > $1.id }
+
+      return resolve(activities.map { ["id": $0.id, "data": $0.contentState.data] })
+
+    } else {
+      resolve([])
     }
-    
-    @objc(endActivity:withResolver:withRejecter:)
-    func endActivity(id: String, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
-        if #available(iOS 16.1, *) {
-            Task {
-                await Activity<MyActivityAttributes>.activities.filter {$0.id == id}.first?.end(dismissalPolicy: .immediate)
-            }
-        } else {
-            reject("Not available","", NSError())
-        }
+  }
+
+  @objc(endActivity:withResolver:withRejecter:)
+  func endActivity(id: String, resolve _: RCTPromiseResolveBlock, reject _: RCTPromiseRejectBlock) {
+    if #available(iOS 16.1, *), isIphone() {
+      Task {
+        await Activity<MyActivityAttributes>.activities.filter { $0.id == id }.first?.end(dismissalPolicy: .immediate)
+      }
     }
-    
-    @objc(updateActivity:withData:withResolver:withRejecter:)
-    func updateActivity(id: String, data: String, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
-        if #available(iOS 16.1, *) {
-            Task {
-                let updatedStatus = MyActivityAttributes
-                    .ContentState(data: data)
-                let activities = Activity<MyActivityAttributes>.activities
-                let activity = activities.filter {$0.id == id}.first
-                await activity?.update(using: updatedStatus)
-            }
-        } else {
-            reject("Not available", "", NSError())
-        }
+  }
+
+  @objc(updateActivity:withData:withResolver:withRejecter:)
+  func updateActivity(id: String, data: String, resolve _: RCTPromiseResolveBlock, reject _: RCTPromiseRejectBlock) {
+    if #available(iOS 16.1, *), isIphone() {
+      Task {
+        let updatedStatus = MyActivityAttributes
+          .ContentState(data: data)
+        let activities = Activity<MyActivityAttributes>.activities
+        let activity = activities.filter { $0.id == id }.first
+        await activity?.update(using: updatedStatus)
+      }
     }
+  }
 }
